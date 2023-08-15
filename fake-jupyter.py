17b68b690091c32fd28d1f5412dee6ed62f88f7d
Owain Kenway <o.kenway@ucl.ac.uk>
2023-08-15 13:01:21 +0100

    Start.

---
+++ fake-jupyter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+    Main command line tool with emulates the Jupyter runner.
+"""
+
+if __name__ == "__main__":
+    import argparse
+
+    print("Nothing implemented.")
